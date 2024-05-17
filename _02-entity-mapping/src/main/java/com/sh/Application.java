1ec24ac4b4db314e67a006d9d1ba7a5f41d07734
nob0dj <dongxuan1863@gmail.com>
2024-05-17 16:26:39 +0900

    Rename: Application 시작클래스명 변경

--- _02-entity-mapping/src/main/java/com/sh/Application.java
+++ _02-entity-mapping/src/main/java/com/sh/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DddApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DddApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
